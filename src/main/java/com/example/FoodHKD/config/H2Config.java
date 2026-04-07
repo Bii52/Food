dcbfdeefb2ea3684f75d52510e58f33fa9ef1124
Bao kiet <baokie2004@gmail.com>
2026-04-07 22:22:35 +0700

    Add AuthenticationManager bean to H2Config for h2 profile

--- src/main/java/com/example/FoodHKD/config/H2Config.java
+++ src/main/java/com/example/FoodHKD/config/H2Config.java
@@ -3,7 +3,10 @@ package com.example.FoodHKD.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.util.Collections;
 
 @Configuration
 @Profile("h2")
@@ -12,5 +15,10 @@ public class H2Config {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(Collections.emptyList());
     }
 }
